0bdd809424410881ccbdfc477edf545d4f123fbe
Maureen T'o <maureenito@gmail.com>
2020-09-18 12:50:26 -0400

    Adjust footer links in mobile

--- easybank-landing-page/main.js
+++ easybank-landing-page/main.js
@@ -1,6 +1,5 @@
 // Selectors
 const navBurger = document.querySelector(".nav-burger");
-// const navBurgerClose = document.querySelector(".nav-burger-close");
 const nav = document.querySelector(".nav-modal");
 
 // Toggle Functions
@@ -8,7 +7,7 @@ const openModal = () => {
 	// Toggle mobile navigation
 	navBurger.addEventListener("click", () => {
 		nav.classList.toggle("modal-active");
-	})
+	});
 };
 
 openModal();
